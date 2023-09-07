abb8212774e59b970eea81b5f81f7fd7f6e638ee
SankalpWr <66629056+SankalpWr@users.noreply.github.com>
2023-09-07 21:39:29 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(ReminderApp());
+
+class ReminderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ReminderScreen(),
+    );
+  }
+}
+
+class ReminderScreen extends StatefulWidget {
+  @override
+  _ReminderScreenState createState() => _ReminderScreenState();
+}
+
+class _ReminderScreenState extends State<ReminderScreen> {
+  final AudioCache audioCache = AudioCache();
+
+  String selectedDay = 'Monday';
+  String selectedTime = '08:00 AM';
+  String selectedActivity = 'Wake up';
+
+  final List<String> daysOfWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  final List<String> timesOfDay = [
+    '08:00 AM',
+    '09:00 AM',
+    '10:00 AM',
+    '11:00 AM',
+    '12:00 PM',
+    '01:00 PM',
+    '02:00 PM',
+    '03:00 PM',
+    '04:00 PM',
+    '05:00 PM',
+    '06:00 PM',
+    '07:00 PM',
+    '08:00 PM',
+    '09:00 PM',
+    '10:00 PM',
+  ];
+
+  final List<String> activities = [
+    'Wake up',
+    'Go to gym',
+    'Breakfast',
+    'Meetings',
+    'Lunch',
+    'Quick nap',
+    'Go to library',
+    'Dinner',
+    'Go to sleep',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reminder App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Select a day of the week:'),
+            DropdownButton<String>(
+              value: selectedDay,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedDay = newValue!;
+                });
+              },
+              items: daysOfWeek.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            Text('Select a time:'),
+            DropdownButton<String>(
+              value: selectedTime,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedTime = newValue!;
+                });
+              },
+              items: timesOfDay.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            Text('Select an activity:'),
+            DropdownButton<String>(
+              value: selectedActivity,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedActivity = newValue!;
+                });
+              },
+              items: activities.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement reminder logic here
+                // Play a sound/chime when the time is up
+                audioCache.play('chime.mp3');
+              },
+              child: Text('Set Reminder'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
